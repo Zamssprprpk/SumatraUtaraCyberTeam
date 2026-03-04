98f5505d0f19c8797a968ce4930dfd3cde21123a
Zamssprprpk <zamsswangsaff@gmail.com>
2026-03-05 06:12:43 +0700

    Create yap.php to display hack message
    This file displays a message indicating a successful hack by a cyber team.

---
+++ yap.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Touched By ZammSec01</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 60px;
+            background-color: black;
+            text-align: center;
+            font-family: monospace;
+            color: white;
+        }
+
+        .uname-result {
+            font-size: 20px;
+            font-weight: 700;
+            color: white;
+        }
+
+        img {
+            margin-top: 35px;
+            max-width: 500px;
+            height: auto;
+            border: 3px solid white;
+        }
+
+        .signature {
+            margin-top: 40px;
+            font-size: 38px;
+            font-weight: 900; /* paling tebal */
+            letter-spacing: 3px;
+        }
+
+        .touched {
+            color: red;
+        }
+
+        .name {
+            color: white;
+        }
+
+        .custom-text {
+            margin-top: 25px;
+            font-size: 18px;
+            font-weight: 700;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="uname-result">
+        Linux server 5.15.0-86-generic #96-Ubuntu SMP x86_64 GNU/Linux
+    </div>
+
+    <img src="https://img.sanishtech.com/u/a331aa0d736815f8f34846b91f61f090.png" alt="Image">
+
+    <div class="signature">
+        <span class="touched">Touched By</span> 
+        <span class="name">./ZammSec01</span>
+    </div>
+
+    <div class="custom-text">
+        Your system was successfully hacked by  Sumatra Utara Cyber Team
+    </div>
+
+</body>
+</html>
